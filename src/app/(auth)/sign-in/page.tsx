1041e78feb2a16f3fa5d2e7f7b901e81a599fe6a
Adarsh Jha <adarshjhaxif@gmail.com>
2024-05-03 22:01:15 +0530

    sign-up ui done, bug in integration

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,30 +1,11 @@
-"use client";
-
-import { signIn, signOut, useSession } from "next-auth/react";
 import React from "react";
 
 const page = () => {
-  const { data: session } = useSession();
-  if (session) {
-    return (
-      <div>
-        <h1>Welcome {session.user.email}</h1>
-        <button onClick={() => signOut()}>Sign Out</button>
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <h1 className="text-white ">Not Signed In</h1>
-        <button
-          className="bg-orange-600 border boder-white px-3 py-1 m-4 rounded-lg"
-          onClick={() => signIn()}
-        >
-          Sign In
-        </button>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>Sign In Page</h1>
+    </div>
+  );
 };
 
 export default page;
